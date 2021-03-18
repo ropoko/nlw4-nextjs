4fcc97e6fc9afd1ef93c73a92adb585e7dd3e5d4
ropoko <rodrigostramantinoli@gmail.com>
2021-03-18 19:22:53 -0300

    day 3

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,36 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/ChallengeBox.module.css'
+
+export function ChallengeBox() {
+    const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+
+    return (
+        <div className={styles.challengeBoxContainer}>
+            { activeChallenge ? (
+                <div className={styles.challengeActive}>
+                    <header>
+                        Ganhe { activeChallenge.amount }
+                    </header>
+                    <main> 
+                        <img src="" alt="body"/>
+                        <strong>Novo desafio</strong>
+                        <p>{activeChallenge.description}</p>
+                    </main>
+                    <footer>
+                        <button onClick={resetChallenge} type="button" className={styles.challengeFailedButton}>Falhei</button>
+                        <button type="button" className={styles.challengeSucceededButton}>Completei</button>
+                    </footer>
+                </div>
+            ) : (
+                <div className={styles.challengeNotActive}>
+                    <strong> Finalize um ciclo para receber um desafio</strong>
+                    <p>
+                        <img src="" alt="level up" />
+                        Avance de level completando desafios
+                    </p>
+                </div>
+            ) }
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { Profile } from '../components/Profile'
 import Head from 'next/head'
 
 import styles from '../styles/pages/Home.module.css'
+import { ChallengeBox } from '../components/ChallengeBox'
 
 export default function Home() {
   return (
@@ -25,7 +26,7 @@ export default function Home() {
         </div>
 
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
